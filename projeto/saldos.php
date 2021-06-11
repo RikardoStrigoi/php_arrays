85afe860e6f06d0bab5494492d20096b9b317b09
Ricardo Ferreira <rikardostrigoi@gmail.com>
2021-06-10 21:27:11 -0300

    Criação arquivo saldos para teste de sort de array

---
+++ projeto/saldos.php
@@ -0,0 +1,19 @@
+<?php
+
+$saldos = [
+  2500,
+  3000,
+  4400,
+  1000,
+  8700,
+  9000
+];
+
+foreach ($saldos as $saldo) {
+  echo "O saldo é $saldo" . PHP_EOL;
+}
+
+// Sort para ordenação crescente de array
+sort($saldos);
+
+echo "O menor saldo é $saldos[0]";
